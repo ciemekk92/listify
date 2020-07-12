269db6da14cacc7636b95759944c977ca1c1482d
ciemekk92 <przemyslaw.reducha@gmail.com>
2020-07-12 18:45:01 +0200

    line separators

--- src/store/actions/list.ts
+++ src/store/actions/list.ts
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-import { Item } from '../../types/'
+import { Item } from '../../types/';
 
 export const setSelectedDate = (date: string) => {
     return {
